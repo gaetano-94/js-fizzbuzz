5327d81ca6d93bc2a19582168caacbea13ba72bd
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-10 17:41:12 +0100

    fix: js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@
 
 //3)BONUS 1)Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 
+//4)BONUS 2)Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+
 const numeroContenitore = document.querySelector('.box');
 
 for (let i = 1; i < 100; i++) {
