7862b500057e26d567bc981928fcf658f1cb4002
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-10 16:28:11 +0100

    feat: multipli di 3 e di 5

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 'use strict';
 
 //1)Scrivi un programma che stampi in console i numeri da 1 a 100
+
+//2)ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+//1)
 let numero100 = 100;
 
-for (let i = 0; i < numero100; i++) {}
+for (let i = 1; i < numero100; i++) {
+  if (i % 3 === 0) {
+    console.log('fizz');
+  } else if (i % 5 === 0) {
+    console.log('buzz');
+  } else {
+    console.log(i);
+  }
+}
