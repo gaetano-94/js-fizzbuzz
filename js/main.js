29416570b4d9382f8bc8ec6435b837173981ca4d
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-10 16:31:30 +0100

    feat: multiplo si di 3 che di 5 e quindi fizzbuzz

--- js/main.js
+++ js/main.js
@@ -8,7 +8,9 @@
 let numero100 = 100;
 
 for (let i = 1; i < numero100; i++) {
-  if (i % 3 === 0) {
+  if (i % 15 === 0) {
+    console.log('fizzbuzz');
+  } else if (i % 3 === 0) {
     console.log('fizz');
   } else if (i % 5 === 0) {
     console.log('buzz');
